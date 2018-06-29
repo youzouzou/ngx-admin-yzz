76df273370d27529194f766b8b3e25a8bb97c2a6
youzouzou <573257706@qq.com>
2018-06-29 12:33:38 +0800

    tab

--- src/app/common/components/tab/tab.component.ts
+++ src/app/common/components/tab/tab.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import {Component, Input, Output, EventEmitter} from '@angular/core';
+import {} from "selenium-webdriver";
 @Component({
   selector: 'tab',
   templateUrl: './tab.component.html',
@@ -7,5 +7,37 @@ import { Component } from '@angular/core';
 })
 
 export class TabComponent {
+  @Input() tabList;
+  @Output() changeTab: EventEmitter<Number> = new EventEmitter;// 子组件向父组件广播事件，触发改变当前页面的事件
+  @Input() curIndex: number = 0;
 
+  constructor() {
+    this.tabList = [
+      {
+        title: '标签页',
+        hidden: false
+      },
+      {
+        title: '标签页',
+        hidden: false
+      },
+      {
+        title: '标签页',
+        hidden: false
+      },
+      {
+        title: '标签页',
+        hidden: false
+      },
+      {
+        title: '标签页',
+        hidden: false
+      }
+    ]
+  }
+
+  changeCurTab(index) {
+    this.curIndex = index;
+    this.changeTab.emit(this.curIndex);
+  }
 }
--- src/app/pages/tab/tab.page.ts
+++ src/app/pages/tab/tab.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'tab-page',
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
 })
 
 export class TabPage {
+  curTabIndex: number = 0;
 
+  getTabData(index) {
+    this.curTabIndex = index;
+  }
 }
