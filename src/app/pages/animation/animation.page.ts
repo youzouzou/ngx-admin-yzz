07635fd2de1d841b1310c18e877c836a85bf2e59
youzouzou <573257706@qq.com>
2018-06-29 17:53:44 +0800

    添加动画

--- src/app/pages/animation/animation.page.ts
+++ src/app/pages/animation/animation.page.ts
@@ -7,4 +7,13 @@ import {Component} from '@angular/core';
 })
 
 export class AnimationPage {
+  boxList = ['linear-show', 'bounce-show', 'rotateY-show'];
+
+  showAgain(event, index) {
+    let dom = event.target;
+    let parent = dom.parentElement;
+    let parent2 = dom.parentElement.parentElement;
+    parent2.removeChild(parent);
+    parent2.appendChild(parent);
+  }
 }
