84cd1843fe0b1b5be868e4a5e4fa3218db5ee68d
youzouzou <573257706@qq.com>
2018-06-30 11:11:49 +0800

    添加文档说明页面

---
+++ src/app/pages/document/document.page.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'document-page',
+  templateUrl: './document.page.html',
+  styleUrls: ['./document.page.css']
+})
+
+export class DocumentPage {
+
+}
