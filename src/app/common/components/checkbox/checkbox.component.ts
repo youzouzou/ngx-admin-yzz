8d7400da33513795c7ac839d04d051bb63f10edc
youzouzou <573257706@qq.com>
2018-06-30 11:11:17 +0800

    添加checkbox组件

---
+++ src/app/common/components/checkbox/checkbox.component.ts
@@ -0,0 +1,10 @@
+import {Component, Input, Output, EventEmitter} from '@angular/core';
+@Component({
+  selector: 'checkbox',
+  templateUrl: './checkbox.component.html',
+  styleUrls: ['./checkbox.component.css']
+})
+
+export class CheckboxComponent {
+
+}
