d015610dd6d15a00950d668f9ffbc5143b5bc485
youzouzou <573257706@qq.com>
2018-06-30 15:17:09 +0800

    checkbox

--- src/app/common/components/checkbox/checkbox.component.ts
+++ src/app/common/components/checkbox/checkbox.component.ts
@@ -6,5 +6,17 @@ import {Component, Input, Output, EventEmitter} from '@angular/core';
 })
 
 export class CheckboxComponent {
+  @Input() checkStatus: boolean | number | string; // 选中状态
+  @Input() checkboxColor: 'white' | 'black'; // 复选框钩子的图标可选中，支持自定义checkIcon，参数值为图标url链接
+  @Input() checkIcon = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAMAAAC6V+0/AAAAXVBMVEUAAAAtJiUSCwpoTk47JyhrVlRPOjl/YGAKBwcGBAQKBwYAAAAWEhIdFBQbFhYiFhYwKSYtIyMsICEyJiUdFBQ1LC1IPDxTQkBTOzthQkCAXFukcHCAaWYAAAAKBgbQXivSAAAAHXRSTlMAp4BWNjAqIffs6OXa18vAt6GPiYF7ZmJaRBYOBU4rugEAAABTSURBVBjTtcg3DoBAFANRk3POYbj/MRHVCn7NKyx59D9/lLGBaXvKZGJNa1pPcboXhs8GELm2XsRSArOc2COXKjy9NHQDpd6ODEj0EUEgY/H1rxsobgP48AG/KwAAAABJRU5ErkJggg==';
+  @Input() checkboxBg = '#fff'; // 复选框的背景色
+  @Input() checkboxSize = 20; // 复选框大小
+  @Output() changeCheckStatus: EventEmitter<boolean | number | string> = new EventEmitter;
 
+  changeStatus(checkStatus) {
+    this.checkStatus = !checkStatus;
+    this.changeCheckStatus.emit(this.checkStatus);
+    event.preventDefault();
+    event.stopPropagation();
+  }
 }
--- src/app/pages/document/document.page.ts
+++ src/app/pages/document/document.page.ts
@@ -7,5 +7,97 @@ import {Component} from '@angular/core';
 })
 
 export class DocumentPage {
+  index = 0;
+  apiList = [
+    {
+      title: '表格',
+      checkStatus: false,
+    },
+    {
+      title: '图表（Chart.js）',
+      checkStatus: true,
+    },
+    {
+      title: '表单',
+      checkStatus: false,
+    },
+    {
+      title: '标签页',
+      checkStatus: false,
+    },
+    {
+      title: '富文本',
+      checkStatus: false,
+    },
+    {
+      title: '上传文件',
+      checkStatus: false,
+    },
+    {
+      title: '图片处理',
+      checkStatus: false,
+    },
+    {
+      title: '顶部&侧边导航栏',
+      checkStatus: false,
+    },
+    {
+      title: '面包屑导航',
+      checkStatus: false,
+    },
+    {
+      title: '下拉框',
+      checkStatus: false,
+    },
+    {
+      title: '搜索框',
+      checkStatus: false,
+    },
+    {
+      title: '模态框',
+      checkStatus: false,
+    },
+    {
+      title: '按钮',
+      checkStatus: false,
+    },
+    {
+      title: '标签输入框',
+      checkStatus: false,
+    },
+    {
+      title: 'tooltip',
+      checkStatus: false,
+    },
+    {
+      title: 'animation',
+      checkStatus: false,
+    },
+    {
+      title: 'validate',
+      checkStatus: false,
+    },
+    {
+      title: 'loading',
+      checkStatus: false,
+    },
+    {
+      title: '小数点精度问题',
+      checkStatus: false,
+    },
+    {
+      title: 'message',
+      checkStatus: false,
+    }
+  ];
 
+  changeStatus(value, index) {
+    // todo 这样还是不太方便，想办法能不能换成双向数据绑定，或者就直接能获取到值
+    console.log('点击改变值，当前为', value, index);
+    this.apiList[index].checkStatus = value;
+  }
+
+  select(index) {
+    this.index = index;
+  }
 }
