2f803acb5c77e29976bfb62ff77a5c198cc2e119
youzouzou <573257706@qq.com>
2018-06-30 11:10:56 +0800

    菜单配置列表不需要引入组件

--- src/app/common/layout/menu.config.ts
+++ src/app/common/layout/menu.config.ts
@@ -1,40 +1,36 @@
-import {TableComponent} from "../../pages/table/table.page";
-import {SmartTablePage} from "../../pages/table/smart-table/smart-table.page";
-import {ChartComponent} from "../../pages/chart/chart.page";
-import {TableDetailPage} from "../../pages/table/detail/detail.page";
-import {TabPage} from "../../pages/tab/tab.page";
-import {AnimationPage} from "../../pages/animation/animation.page";
-
 const menuList:any = [
   {
     title: '表格', path: 'table',
     children: [
       {
-        title: '基本表格', path: 'basic_table', component: TableComponent,
+        title: '基本表格', path: 'basic_table',
         children: [
           {
-            path: 'table_detail', title: '表格详情', component: TableDetailPage
+            path: 'table_detail', title: '表格详情'
           }
         ]
       },
       {
-        title: '智能表格', path: 'smart_table', component: SmartTablePage,
+        title: '智能表格', path: 'smart_table',
         children: [
           {
-            path: 'table_detail', title: '表格详情', component: TableDetailPage
+            path: 'table_detail', title: '表格详情'
           }
         ]
       }
     ]
   },
   {
-    path: 'chart', title: '图表', component: ChartComponent
+    path: 'chart', title: '图表'
   },
   {
-    path: 'tab', title: '标签页', component: TabPage
+    path: 'tab', title: '标签页'
   },
   {
-    path: 'animation', title: '动画', component: AnimationPage
+    path: 'animation', title: '动画'
+  },
+  {
+    path: 'document', title: '文档'
   }
 ];
 
