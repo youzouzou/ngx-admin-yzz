195e38fbae4b851b2f2ed2428c373db91a123475
youzouzou <youzouzou@youzouzoudeMacBook-Pro.local>
2018-07-01 01:04:55 +0800

    radio

--- src/app/common/components/radio/radio.component.ts
+++ src/app/common/components/radio/radio.component.ts
@@ -5,18 +5,26 @@ import {Component, Input, Output, EventEmitter, OnInit} from '@angular/core';
   styleUrls: ['./radio.component.css']
 })
 
-export class RadioComponent {
+export class RadioComponent implements DoCheck {
   @Input() radioData = [{}];
+  @Input() radioLable;
   @Input() selectedValue; // 初始化默认选中值，若要生效，必须设置用于判断的字段名selectedName
-  @Input() selectedName: string;// 初始化默认选中值的字段名
-  @Input() selectedIndex: number;// 初始化默认选中值的下标，可为空
+  @Input() selectedName:string;// 初始化默认选中值的字段名
+  @Input() selectedIndex:number;// 初始化默认选中值的下标，可为空
   @Input() radioSize = 20;
   @Input() radioBg = '#fff';
   @Input() radioColor = '#ccc';
-  @Output() changeRadio: EventEmitter<any> = new EventEmitter;
+  @Output() changeRadio:EventEmitter<any> = new EventEmitter;
 
-  ngOnInit() {
-    if(this.selectedName){
+  ngDoCheck() { // todo 什么时候触发这个？
+    this.setData();
+  }
+
+  setData() {
+    if (!this.radioLable && this.radioData.length) {
+      this.radioLable = '0';
+    }
+    if (this.selectedName) {
       for (let i = 0; i < this.radioData.length; i++) {
         if (this.selectedValue == this.radioData[i][this.selectedName]) {
           this.selectedIndex = i;
