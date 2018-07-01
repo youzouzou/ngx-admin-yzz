9edf8a79967f7b9bcd22afd2c0afc999a0bb812e
youzouzou <573257706@qq.com>
2018-07-01 10:19:22 +0800

    radio

--- src/app/common/components/radio/radio.component.ts
+++ src/app/common/components/radio/radio.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, Output, EventEmitter, OnInit} from '@angular/core';
+import {Component, Input, Output, EventEmitter, OnInit, DoCheck} from '@angular/core';
 @Component({
   selector: 'radio',
   templateUrl: './radio.component.html',
