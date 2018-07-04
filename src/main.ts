a47b06c4fb3ae19fc665a81f98b1f590d2967f1e
youzouzou <573257706@qq.com>
2018-07-04 17:59:15 +0800

    控制台日志打印

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import {AppModule} from './app/app.module';
+import {environment} from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+}
+
+let status = false; // 是否开启控制台打印日志
+if (status) {
+  window.console.log = () => {};
+  window.console.info = () => {};
+  window.console.warn = () => {};
+  window.console.error = () => {};
+  window.console.debug = () => {};
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
