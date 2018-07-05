53f98e18db6a1b713cb29c1635244159ef717a70
youzouzou <573257706@qq.com>
2018-07-05 16:20:47 +0800

    修复active样式问题

--- src/app/common/layout/topNavbar/topNavbar.component.ts
+++ src/app/common/layout/topNavbar/topNavbar.component.ts
@@ -10,7 +10,7 @@ import {NavigationEnd, Router} from "@angular/router";
 export class TopNavbarComponent {
   menuList = menuConfig.menuList;
   screenWidth = window.innerWidth;
-  curPathIndex: number = 0;
+  curPathIndex: number;
   prePathIndex: number = 0;
   render2: any;
   curParent: string;
@@ -23,28 +23,32 @@ export class TopNavbarComponent {
   ngOnInit() {
     this.router.events
       .subscribe((event) => {
-        if (event instanceof NavigationEnd) {
-          let paths = this.router.url.split('/');
-          console.log('paths', paths);
-          if (paths.length > 1 && paths[1]) {
-            this.curParent = paths[1];
-            if (paths.length > 2) {
-              this.curChild = paths[2];
-            }
-            for (let i = 0; i < this.menuList.length; i++) {
-              if (this.menuList[i].path === paths[1]) {
-                this.prePathIndex = 0;
-                this.curPathIndex = i;
-                this.render2.selectRootElement('.top-menu-item-active').style.left = i * 100 + 'px';
-                break;
+        if (!this.curPathIndex && this.curPathIndex!=0) {
+          if (event instanceof NavigationEnd) {
+            let paths = this.router.url.split('/');
+            console.log('paths', paths);
+            if (paths.length > 1 && paths[1]) {
+              this.curParent = paths[1];
+              if (paths.length > 2) {
+                this.curChild = paths[2];
               }
+              for (let i = 0; i < this.menuList.length; i++) {
+                if (this.menuList[i].path === paths[1]) {
+                  this.prePathIndex = 0;
+
+                  this.curPathIndex = i;
+                  this.render2.selectRootElement('.top-menu-item-active').style.left = i * 100 + 'px';
+
+                  break;
+                }
+              }
+            } else {
+              this.curParent = menuConfig.menu.path;
+              if (menuConfig.menu.children && menuConfig.menu.children.length) {
+                this.curChild = menuConfig.menu.children[0].path;
+              }
+              // console.log('初始化默认菜单', this.curParent, this.curChild);
             }
-          } else {
-            this.curParent = menuConfig.menu.path;
-            if (menuConfig.menu.children && menuConfig.menu.children.length) {
-              this.curChild = menuConfig.menu.children[0].path;
-            }
-            // console.log('初始化默认菜单', this.curParent, this.curChild);
           }
         }
       });
@@ -55,6 +59,7 @@ export class TopNavbarComponent {
     this.curPathIndex = i;
     let render2 = this.render2;
     // 设置移动动画
+    console.log('移动', this.prePathIndex, this.curPathIndex);
     let dom = render2.selectRootElement('.top-menu-item-active');
     if (this.prePathIndex < this.curPathIndex) {
       for (let j = this.prePathIndex * 100; j <= this.curPathIndex * 100; j++) {
@@ -69,7 +74,7 @@ export class TopNavbarComponent {
         }, 100);
       }
     }
-    let paths = this.router.url.split('/'); // todo 这一步初始化当前路由，这样取值可能会有问题
+    let paths = this.router.url.split('/');
     if (paths.length > 1) {
       this.curParent = paths[1];
       if (paths.length > 2) {
