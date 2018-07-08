2711b6168634dbab3487e28f1a93f5a0dfc25175
youzouzou <youzouzou@youzouzoudeMacBook-Pro.local>
2018-07-08 17:55:24 +0800

    api注入bug

--- src/app/common/service/api.service.ts
+++ src/app/common/service/api.service.ts
@@ -2,11 +2,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class apiService {
-  constructor() {
-    // 接口url集中在此
-    return {
-      cities:'assets/data/city.json',
-      upload: 'http://upload/picture'
-    }
-  }
+  cities = 'assets/data/city.json';
+  upload = 'http://upload/picture';
 }
