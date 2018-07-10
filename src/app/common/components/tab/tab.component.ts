a1857f9acca55ebb947ffc2c215bc08086a95f33
youzouzou <573257706@qq.com>
2018-07-10 17:13:38 +0800

    增加可选样式：flat 扁平无圆角风格

--- src/app/common/components/tab/tab.component.ts
+++ src/app/common/components/tab/tab.component.ts
@@ -7,33 +7,9 @@ import {Component, Input, Output, EventEmitter} from '@angular/core';
 
 export class TabComponent {
   @Input() tabList;
-  @Output() changeTab: EventEmitter<Number> = new EventEmitter;// 子组件向父组件广播事件，触发改变当前页面的事件
   @Input() curIndex: number = 0;
-
-  constructor() {
-    this.tabList = [
-      {
-        title: '标签页',
-        hidden: false
-      },
-      {
-        title: '标签页',
-        hidden: false
-      },
-      {
-        title: '标签页',
-        hidden: false
-      },
-      {
-        title: '标签页',
-        hidden: false
-      },
-      {
-        title: '标签页',
-        hidden: false
-      }
-    ]
-  }
+  @Input() style: string;
+  @Output() changeTab: EventEmitter<Number> = new EventEmitter;// 子组件向父组件广播事件，触发改变当前页面的事件
 
   changeCurTab(index) {
     this.curIndex = index;
