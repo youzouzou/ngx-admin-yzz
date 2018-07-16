20708b440b7473d2c4a86927a6f44788fe5b7fd8
youzouzou <573257706@qq.com>
2018-07-16 17:07:44 +0800

    http请求封装

--- src/app/common/service/selfhttp.service.ts
+++ src/app/common/service/selfhttp.service.ts
@@ -13,7 +13,7 @@ export class selfHttp {
 
   public get(url, params?: Object, cb?: Function) {
     let httpParams = new HttpParams();
-    console.log('开始请求')
+    console.log('get开始请求');
     const vm = this;
     if (params) {
       for (const key in params) {
@@ -24,7 +24,45 @@ export class selfHttp {
     }
     vm.http.get(vm.restServer + url, {params: httpParams})
       .subscribe(data => {
-        console.log('请求结束', data)
+        console.log('get请求结束', data);
+        cb(data);
+      });
+  }
+
+  public post(url, data?: Object, cb?: Function, options?: Object) {
+    console.log('post开始请求');
+    const vm = this;
+    vm.http.post(vm.restServer + url, data, options)
+      .subscribe(res => {
+        console.log('post请求结束', res);
+        cb(res);
+      });
+  }
+
+  public put(url, data?: Object, cb?: Function, options?: Object) {
+    console.log('put开始请求');
+    const vm = this;
+    vm.http.put(vm.restServer + url, data, options)
+      .subscribe(res => {
+        console.log('put请求结束', res);
+        cb(res);
+      });
+  }
+
+  public delete(url, params?: Object, cb?: Function) {
+    let httpParams = new HttpParams();
+    console.log('delete开始请求');
+    const vm = this;
+    if (params) {
+      for (const key in params) {
+        if (params[key]) {
+          httpParams = httpParams.set(key, params[key]);
+        }
+      }
+    }
+    vm.http.delete(vm.restServer + url, {params: httpParams})
+      .subscribe(data => {
+        console.log('delete请求结束', data);
         cb(data);
       });
   }
