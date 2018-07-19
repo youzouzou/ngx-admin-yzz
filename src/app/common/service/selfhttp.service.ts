1fdf733fa4a6e48d411845ed2a5f43e6edcf5e21
youzouzou <573257706@qq.com>
2018-07-19 17:10:19 +0800

    修改http请求中值为false的参数的bug

--- src/app/common/service/selfhttp.service.ts
+++ src/app/common/service/selfhttp.service.ts
@@ -1,12 +1,13 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
+import {globalService} from "./global.service";
 
 @Injectable()
 export class selfHttp {
   public restServer;
   public http;
 
-  constructor(Http: HttpClient) {
+  constructor(Http: HttpClient, public global: globalService) {
     this.http = Http;
     this.restServer = 'http://127.0.0.0/';
   }
@@ -14,10 +15,11 @@ export class selfHttp {
   public get(url, params?: Object, cb?: Function) {
     let httpParams = new HttpParams();
     console.log('get开始请求');
+    this.global.loadStatus = true;
     const vm = this;
     if (params) {
       for (const key in params) {
-        if (params[key]) {
+        if (params[key] === false || params[key]) {
           httpParams = httpParams.set(key, params[key]);
         }
       }
@@ -25,26 +27,31 @@ export class selfHttp {
     vm.http.get(vm.restServer + url, {params: httpParams})
       .subscribe(data => {
         console.log('get请求结束', data);
+        vm.global.loadStatus = false;
         cb(data);
       });
   }
 
   public post(url, data?: Object, cb?: Function, options?: Object) {
     console.log('post开始请求');
+    this.global.loadStatus = true;
     const vm = this;
     vm.http.post(vm.restServer + url, data, options)
       .subscribe(res => {
         console.log('post请求结束', res);
+        vm.global.loadStatus = false;
         cb(res);
       });
   }
 
   public put(url, data?: Object, cb?: Function, options?: Object) {
     console.log('put开始请求');
+    this.global.loadStatus = true;
     const vm = this;
     vm.http.put(vm.restServer + url, data, options)
       .subscribe(res => {
         console.log('put请求结束', res);
+        vm.global.loadStatus = false;
         cb(res);
       });
   }
@@ -52,6 +59,7 @@ export class selfHttp {
   public delete(url, params?: Object, cb?: Function) {
     let httpParams = new HttpParams();
     console.log('delete开始请求');
+    this.global.loadStatus = true;
     const vm = this;
     if (params) {
       for (const key in params) {
@@ -63,6 +71,7 @@ export class selfHttp {
     vm.http.delete(vm.restServer + url, {params: httpParams})
       .subscribe(data => {
         console.log('delete请求结束', data);
+        vm.global.loadStatus = false;
         cb(data);
       });
   }
